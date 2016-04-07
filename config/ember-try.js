88f43269c73611cb5d5bfaf7da1e757427458dc2
Gastón I. Silva <givanse@gmail.com>
2016-04-07 13:42:00 -0700

    support ember 1.13.13 and 1.12.2

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,19 +2,24 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
+      name: 'ember-1.12.2',
       bower: {
-        dependencies: { }
+        dependencies: {
+          'ember': '~1.12.2'
+        },
+        resolutions: {
+          'ember': '~1.12.2'
+        }
       }
     },
     {
-      name: 'ember-1.13',
+      name: 'ember-1.13.13',
       bower: {
         dependencies: {
-          'ember': '~1.13.0'
+          'ember': '~1.13.13'
         },
         resolutions: {
-          'ember': '~1.13.0'
+          'ember': '~1.13.13'
         }
       }
     },
