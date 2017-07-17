60542bb6209fa43ef86b2e914632d967691e65ff
eagle.dan <eagle.dan.1349@gmail.com>
2017-07-17 18:13:50 +0600

    How about a more Ember way of event throughput?

--- addon/components/ember-zingchart.js
+++ addon/components/ember-zingchart.js
@@ -1,19 +1,130 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-	classNames:['zingchart-ember'],
+// https://www.zingchart.com/docs/api/events/
+const EVENT_NAMES = [
+// Animation Events
+  'animation_end',
+  'animation_start',
+  'animation_step',
+// Data Manipulation Events
+  'modify',
+  'node_add',
+  'node_remove',
+  'plot_add',
+  'plot_modify',
+  'plot_remove',
+  'reload',
+  'setdata',
+// Export Events
+  'data_export',
+  'image_save',
+  'print',
+// Feed Events
+  'feed_clear',
+  'feed_interval_modify',
+  'feed_start',
+  'feed_stop',
+// Global Events
+  'click',
+  'complete',
+  'dataparse',
+  'dataready',
+  'guide_mousemove',
+  'load',
+  'menu_item_click',
+  'resize',
+// Graph Events
+  'gcomplete',
+  'gload',
+// History Events
+  'history_back',
+  'history_forward',
+// History Events
+  'node_deselect',
+  'node_select',
+  'plot_deselect',
+  'plot_select',
+// Legend Events
+  'legend_item_click',
+  'legend_marker_click',
+// Node Events
+  'node_click',
+  'node_doubleclick',
+  'node_mouseout',
+  'node_mouseover',
+// Object Events
+  'label_click',
+  'label_mouseout',
+  'label_mouseover',
+  'legend_marker_click',
+  'shape_click',
+  'shape_mouseout',
+  'shape_mouseover',
+// Plot Events
+  'plot_click',
+  'plot_doubleclick',
+  'plot_mouseout',
+  'plot_mouseover',
+// Toggle Events
+  'about_hide',
+  'about_show',
+  'bugreport_hide',
+  'bugreport_show',
+  'dimension_change',
+  'legend_hide',
+  'legend_maximize',
+  'legend_minimize',
+  'legend_show',
+  'lens_hide',
+  'lens_show',
+  'plot_hide',
+  'plot_show',
+  'source_hide',
+  'source_show',
+// Zoom Events
+  'zoom'
+];
 
-	renderOptions: undefined,
+export default Ember.Component.extend({
+  classNames:['zingchart-ember'],
+
+  _eventDispatcher: function (handlerName, event, data) {
+
+    const handler = this.get(handlerName);
+
+    if (handler) {
+      return handler(event, data);
+    }
+    else {
+      return data;
+    }
+  },
+
+  _events: Ember.computed(function(){
+
+    const events = {};
+    const eventDispatcher = this.get('_eventDispatcher');
+
+    EVENT_NAMES.forEach(
+      eventName => {
+        const handlerName = `on_${eventName}`;
+        events[eventName] = eventDispatcher.bind(this, handlerName.camelize());
+      });
+
+    return events;
+  }),
+
+  renderOptions: undefined,
 
   // http://www.zingchart.com/docs/developers/zingchart-object-and-methods/#render-method
   _defaultRenderOptions: {
     // minimal config
-	  id: null,
+    id: null,
     data: null,
     height: 400,
     width: 600,
     // optional defaults
-	  output: 'svg',
+    output: 'svg',
     hideprogresslogo: true,
     'type': null,
     'labels': [
@@ -38,6 +149,7 @@ export default Ember.Component.extend({
 
   _overwriteDefaultRenderOptions: function(renderOptions) {
     this._defaultRenderOptions.id = this.elementId;
+    this._defaultRenderOptions.events = this.get('_events');
 
     // Overwrite the defaults with the passed config
     for (let key of Object.keys(renderOptions)) {
@@ -47,23 +159,23 @@ export default Ember.Component.extend({
     return this._defaultRenderOptions;
   },
 
-	didInsertElement: function() {
-		this.renderLater();
-	},
+  didInsertElement: function() {
+    this.renderLater();
+  },
 
-	renderLater:function() {
-		Ember.run.scheduleOnce('afterRender', this, '_renderChart');
-	},
+  renderLater:function() {
+    Ember.run.scheduleOnce('afterRender', this, '_renderChart');
+  },
 
-	_renderChart: function() {
+  _renderChart: function() {
     let renderOptions = this.get('renderOptions');
     renderOptions = this._overwriteDefaultRenderOptions(renderOptions);
-		zingchart.render(renderOptions);
-	},
+    zingchart.render(renderOptions);
+  },
 
-	_destroyChart: Ember.on('willDestroyElement', function() {
-		var self=this;
-		zingchart.exec(self.elementId,'destroy');
-	})
+  _destroyChart: Ember.on('willDestroyElement', function() {
+    var self=this;
+    zingchart.exec(self.elementId,'destroy');
+  })
 
 });
