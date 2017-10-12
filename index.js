1683383565f5d4ad2244c3479d5849dc9b006f26
eagle.dan <eagle.dan.1349@gmail.com>
2017-10-12 17:37:59 +0600

    Updating gitignore Fixing module imports

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/zingchart/client/zingchart.min.js');
+    app.import(app.bowerDirectory + '/zingchart/client/modules/*.js');
   }
 };
